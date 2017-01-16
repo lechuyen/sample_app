922c09756601a986c165f8df86d2ec1dce216ef2
lechuyen <chuyenle.1704@gmail.com>
2017-01-16 21:11:53 +0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, except: [:show, :new, :create]
   before_action :admin_user, only: :destroy
   before_action :find_user, except: [:index, :new, :create]
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.activated.paginate page: params[:page]
   end
 
   def new
@@ -15,15 +15,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new
     end
-  end
+  end  
 
   def show
+    redirect_to root_url and return unless @user.activated?
   end
 
   def edit
@@ -41,10 +42,10 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    redirect_to root_url
   end
 
-  private
+  private 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
@@ -52,7 +53,7 @@ class UsersController < ApplicationController
 
   def find_user
     @user = User.find_by id: params[:id]
-    if @user.nil?
+    unless @user
       flash[:warning] = "Couldn't find this user. Please try again!"
       redirect_to root_url
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,18 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
-  before_save :email_downcase
+  attr_accessor :remember_token, :activation_token
+  before_save :email_dowcase
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, allow_nil: true, length: {minimum: 6}
 
   has_secure_password
 
-  def remember
-    self.remember_token = User.new_token
-    update_attributes remember_digest: User.digest(remember_token)
-  end
-
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password? remember_token
-  end
-
-  def forget
-    update_attributes remember_digest: nil
-  end
-
-  def current_user? user
-    self == user    
-  end
+  scope :activated, -> {where activated: true}
 
   class << self
     def digest string
@@ -43,8 +26,45 @@ class User < ApplicationRecord
     end
   end
 
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+  
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def current_user? user
+    self == user
+  end
+
+  def activate
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
-  def email_downcase
+  def email_dowcase
     email.downcase!
+  end
+  
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
