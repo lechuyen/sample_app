035ec11992d3201664ad7db5ba2bd7001203f36c
lechuyen <chuyenle.1704@gmail.com>
2017-02-07 19:07:35 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :admin_user, only: :destroy
   before_action :find_user, except: [:index, :new, :create]
   before_action :correct_user, only: [:edit, :update]
@@ -24,7 +24,8 @@ class UsersController < ApplicationController
   end  
 
   def show
-    redirect_to root_url and return unless @user.activated?
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def edit
@@ -68,6 +69,7 @@ class UsersController < ApplicationController
   end
 
   def correct_user
+    @user = User.find(params[:id])
     redirect_to root_url unless current_user.current_user? @user
   end
 
